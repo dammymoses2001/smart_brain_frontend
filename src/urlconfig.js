d3291372eaa6e68fab484fa74541f3a727a6cd34
Dammy <dammymoses2001@gmail.com>
2020-11-12 09:34:50 +0100

    update

--- src/urlconfig.js
+++ src/urlconfig.js
@@ -1,4 +1,5 @@
-const url = 'http://localhost:3000/'
+const url = 'https://smart-brain-app1-0.herokuapp.com/'
 //https://mysterious-eyrie-80862.herokuapp.com/
+//https://smart-brain-app1-0.herokuapp.com/
 //http://localhost:3000/
 export { url }
